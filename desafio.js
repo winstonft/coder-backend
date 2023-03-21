11529e3d3cb1181ec63cb451003d8bbf0ba24ce0
Winston Mendez <winstonmendezft@gmail.com>
2023-03-20 21:33:34 -0300

    first commit

---
+++ desafio.js
@@ -0,0 +1,97 @@
+class TickeyManager {
+    constructor() {
+        this.eventos = []
+    }
+
+    static precioBaseDeGanancia = 1.15;
+
+    getEventos() {
+        return this.eventos;
+    }
+
+    agregarEventos(nombre, lugar, precio) {
+        let id_evento = (this.getEventos()).length;
+
+        let evento = {
+            nombre: nombre,
+            lugar: lugar,
+            precio: precio * TickeyManager.precioBaseDeGanancia,
+            capacidad: 50,
+            fecha: Date(),
+            participantes: [],
+            id: ++id_evento
+        }
+
+        this.eventos.push(evento);
+        return this.eventos;
+    }
+
+    traerEvento(id_evento) {
+        let evento = this.eventos.find(evento => evento.id == id_evento)
+
+        if(evento){
+            return evento;
+        } else {
+            return null
+        }   
+    }
+
+    estaRegistrado(id_evento, id_usuario) {
+        let evento = this.traerEvento(id_evento);
+
+        if(evento == null) {
+            return `El evento no existe`;   
+        }
+
+        let registro = evento.participantes.find(idParticipante => idParticipante === id_usuario)
+
+        if(registro == null) {
+            return true;
+        } else {
+            return false
+        }
+    }
+    
+    agregarUsuario(id_evento, id_usuario) {
+        let evento = this.traerEvento(id_evento);
+
+        if(evento == null) {
+            return 'El evento no existe';
+        }
+        if(this.estaRegistrado(id_evento, id_usuario)) {
+            evento.participantes.push(id_usuario)
+            return evento;
+        } else {
+            return 'La persona ya esta registrada' 
+        }
+    }
+
+    ponerEventoEnGira(id_evento, nLocalidad, nFecha) {
+        let evento = this.traerEvento(id_evento);
+        let contEventos = (this.getEventos()).length;
+
+        if(!evento) {
+            return 'El evento no existe';
+        }
+
+        let nuevoEvento = {...eventos};
+        nuevoEvento.lugar = nLocalidad;
+        nuevoEvento.fecha = nFecha;
+        nuevoEvento.id = ++contEventos;
+        nuevoEvento.participantes = [];
+
+        this.eventos.push(nuevoEvento);
+        return this.eventos;
+    }
+}
+
+const tickeyManager = new TickeyManager();
+tickeyManager.agregarEvento('Rock in Rio', 'Brasil', 1500);
+let eventos = tickeyManager.agregarEventos('Bunker Bar', 'Barquito', 1500);
+//console.log(tickeyManager.traerEvento(1))
+console.log(eventos);
+
+console.log(tickeyManager.agregarUsuario(1,1))
+console.log(tickeyManager.agregarUsuario(1,1))
+console.log(tickeyManager.agregarUsuario(2,1))
+console.log(tickeyManager.agregarUsuario(1,2))
